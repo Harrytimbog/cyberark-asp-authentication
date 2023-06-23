9e4b3066d095ec50bcb6a83ec1981c929f25198c
Harrytimbog <Harriemannie@gmail.com>
2023-06-23 14:38:53 +0100

    Added more fields to Identity

--- cyberark-asp-authentication/Areas/Identity/Data/ApplicationUser.cs
+++ cyberark-asp-authentication/Areas/Identity/Data/ApplicationUser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
@@ -9,5 +10,30 @@ namespace cyberark_asp_authentication.Areas.Identity.Data;
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser
 {
+    [PersonalData]
+    [Column(TypeName = "nvarchar(100)")]
+    public string FirstName { get; set; }
+
+    [PersonalData]
+    [Column(TypeName = "nvarchar(100)")]
+    public string LastName { get; set; }
+
+    [PersonalData]
+    [Column(TypeName = "nvarchar(100)")]
+    public string Account { get; set; }
+
+    [PersonalData]
+    [Column(TypeName = "nvarchar(100)")]
+    public string StaffId { get; set; }
+
+    [PersonalData]
+    [Column(TypeName = "nvarchar(100)")]
+    public string Gender { get; set; }
+
+    [PersonalData]
+    [Column(TypeName = "nvarchar(100)")]
+    public string Department { get; set; }
+
+
 }
 
